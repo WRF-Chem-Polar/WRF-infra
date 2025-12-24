5e2dd83c97c23036c5c3e05992d1963ca00a36b6
Lucas Bastien <134368131+lucas-ige@users.noreply.github.com>
2025-12-24 11:30:18 +0100

    Update the environment file for Spirit

--- env/spirit.sh
+++ env/spirit.sh
@@ -5,5 +5,23 @@
 # License: BSD 3-clause "new" or "revised" license (BSD-3-Clause).
 #
 # Environment for WRF on Spirit.
+#
+# This file is meant to be sourced, not executed.
+#
 
-module load /proju/wrf-chem/software/libraries/gcc-v9.4.0/netcdf-fortran-v4.6.2_netcdf-c-v4.9.3_hdf5-v1.14.6_zlib-v1.3.1.module
+version_gcc=$(gcc --version | head -n 1 | awk '{print $NF}')
+version_gfortran=$(gfortran --version | head -n 1 | awk '{print $NF}')
+if [[ "$version_gcc" != "$version_gfortran" ]]; then
+    echo "Error: GCC and GFORTRAN versions mismatch."
+    exit 1
+fi
+dir_libs=/proju/wrf-chem/software/libraries
+which_netcdf=netcdf-fortran-v4.6.2_netcdf-c-v4.9.3_hdf5-v1.14.6_zlib-v1.3.1.module
+
+module purge
+module load ${dir_libs}/gcc-v${version_gcc}/${which_netcdf}
+
+unset version_gcc
+unset version_gfortran
+unset dir_libs
+unset which_netcdf
