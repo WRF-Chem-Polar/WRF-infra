914d3695b0c3e5f96d4f4bb058d51eee92978853
Lucas Bastien <134368131+lucas-ige@users.noreply.github.com>
2025-12-24 16:42:34 +0100

    Fix environment file for Spirit

--- env/spirit.sh
+++ env/spirit.sh
@@ -10,19 +10,6 @@
 #
 
 module purge
-
-version_gcc=$(gcc --version | head -n 1 | awk '{print $NF}')
-version_gfortran=$(gfortran --version | head -n 1 | awk '{print $NF}')
-if [[ "$version_gcc" != "$version_gfortran" ]]; then
-    echo "Error: GCC and GFORTRAN versions mismatch."
-    exit 1
-fi
-dir_libs=/proju/wrf-chem/software/libraries
-which_netcdf=netcdf-fortran-v4.6.2_netcdf-c-v4.9.3_hdf5-v1.14.6_zlib-v1.3.1.module
-
-module load ${dir_libs}/gcc-v${version_gcc}/${which_netcdf}
-
-unset version_gcc
-unset version_gfortran
-unset dir_libs
-unset which_netcdf
+module load gcc/11.2.0
+module load openmpi/4.0.7
+module load /proju/wrf-chem/software/libraries/gcc-v11.2.0/netcdf-fortran-v4.6.2_netcdf-c-v4.9.3_hdf5-v1.14.6_zlib-v1.3.1.module
