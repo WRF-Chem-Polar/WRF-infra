439be557b5a92b5311c2b5323753ab23028ded98
Lucas Bastien <134368131+lucas-ige@users.noreply.github.com>
2026-01-13 15:40:21 +0100

    Update compile/compile_WPS.py
    Co-authored-by: ekwaly <96921826+ekwaly@users.noreply.github.com>

--- compile/compile_WPS.py
+++ compile/compile_WPS.py
@@ -60,7 +60,7 @@ def write_job_script(opts):
     # Platform-specific environment
     with open(envfile) as f:
         env = [line.strip() for line in f]
-    lines += [line for line in env if line != "" and not line.startswith("#")]
+    lines += [line for line in env if line and not line.startswith("#")]
     lines += prepare_environment_variables(opts)
     setup = {"spirit": 1 + opts.parallel}[host]
 
