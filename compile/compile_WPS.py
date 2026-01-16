77a78200074214539526d828d56423da016d4b06
Lucas Bastien <134368131+lucas-ige@users.noreply.github.com>
2026-01-16 11:10:26 +0100

    Create WRF and WPS compile scripts (#48)
    This commit introduces:
    
     - a WRF/WRF-Chem compile script
     - a WPS compile script
     - a script to install conda environments for WRF-infra
    
    All three are written in Python.
    
    Some of the design principles that went into these scripts are:
    
     - they should be straight-forward for users to use.
     - they should be convenient to use for automatic testing of
       our WRF-Chem-Polar developments
     - they should favor reproducible science (for example by writing
       reproducibility information into the destination directory.
    
    This fixes issue #1.
    
    ---------
    
    Co-authored-by: ekwaly <96921826+ekwaly@users.noreply.github.com>

---
+++ compile/compile_WPS.py
@@ -0,0 +1,98 @@
+# Copyright (c) 2025 LATMOS (France, UMR 8190) and IGE (France, UMR 5001).
+#
+# License: BSD 3-clause "new" or "revised" license (BSD-3-Clause).
+
+"""Compile WPS."""
+
+import sys
+import os.path
+import commons as cms
+
+
+def prepare_environment_variables(opts):
+    """Prepare the compilation environment variables.
+
+    Parameters
+    ----------
+    opts: Namespace
+        The pre-processed user-defined installation options.
+
+    Returns
+    -------
+    [str]
+        The lines setting environment variables.
+
+    """
+    env_vars = {
+        "NETCDF": "$NETCDF_FORTRAN_ROOT",
+        "HDF5": "$HDF5_ROOT",
+        "WRF_DIR": cms.process_path(opts.wrfdir),
+    }
+    return [
+        f"export {name}={cms.format_shell_value(value)}"
+        for name, value in env_vars.items()
+    ]
+
+
+def write_job_script(opts):
+    """Create the job script.
+
+    Parameters
+    ----------
+    opts: Namespace
+        The pre-processed user-defined installation options.
+
+    """
+    # Platform, directories, and files
+    host = cms.identify_host_platform()
+    envfile = os.path.join(cms.path_of_repo(), "env", f"{host}.sh")
+    script = os.path.join(opts.destination, "compile.job")
+
+    # Prepare header of file (hash bang and scheduler options)
+    lines = ["#!/bin/bash"]
+    if opts.scheduler:
+        if host in ("spirit",):
+            lines += cms.prepare_slurm_options("00:30:00")
+        else:
+            msg = f"Unsupported host: {host}."
+            raise NotImplementedError(msg)
+
+    # Platform-specific environment
+    with open(envfile) as f:
+        env = [line.strip() for line in f]
+    lines += [line for line in env if line and not line.startswith("#")]
+    lines += prepare_environment_variables(opts)
+    setup = {"spirit": 1 + opts.parallel}[host]
+
+    # Add the call to ./configure and ./compile
+    lines.append('echo -e "%d" | \\' % setup)
+    lines.append("./configure --build-grib2-libs")
+    lines.append("./compile")
+
+    # Write the script
+    with open(script, mode="x") as f:
+        f.write("\n".join(lines))
+        f.write("\n")
+    os.chmod(script, 0o744)
+
+
+if __name__ == "__main__":
+    # Actually do the work (parse user options, checkout, compile)
+
+    host = cms.identify_host_platform()
+    opts = cms.get_options("WPS")
+
+    cms.clone_and_checkout(opts)
+    cms.write_options(opts)
+    write_job_script(opts)
+    cms.process_patches(opts)
+    cms.process_extra_sources(opts)
+
+    if opts.dry:
+        sys.exit(0)
+
+    if opts.scheduler:
+        cmd = [{"spirit": "sbatch"}[host], "compile.job"]
+    else:
+        cmd = ["./compile.job"]
+    cms.run(cmd, cwd=opts.destination)
---
+++ env/spirit.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+#
+# Copyright (c) 2025 LATMOS (France, UMR 8190) and IGE (France, UMR 5001).
+#
+# License: BSD 3-clause "new" or "revised" license (BSD-3-Clause).
+#
+# Environment for WRF on Spirit.
+#
+# This file is meant to be sourced, not executed.
+#
+
+module purge
+module load /proju/wrf-chem/software/libraries/gcc-v11.2.0/netcdf-fortran-v4.6.2_netcdf-c-v4.9.3_hdf5-v1.14.6_zlib-v1.3.1.module
