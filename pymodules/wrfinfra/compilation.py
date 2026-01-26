f83b3455a8dc50e712082c06a4541617dbd8c56c
Lucas Bastien <134368131+lucas-ige@users.noreply.github.com>
2026-01-26 15:23:37 +0100

    Move content of compile/commons.py to somewhere usable by other scripts (#117)
    Some of the functionality of compile/commons.py can be useful to
    scripts other than those in compile/.
    
    This commit moves content of compile/commons.py to pymodules/wrfinfra/*.
    
    This module can be imported from other locations in this repository.
    
    This commit does not change any actual code.
    
    This commit fixes issue #116.

--- pymodules/wrfinfra/compilation.py
+++ pymodules/wrfinfra/compilation.py
@@ -1,199 +1,13 @@
-# Copyright (c) 2025 LATMOS (France, UMR 8190) and IGE (France, UMR 5001).
+# Copyright (c) 2025-now LATMOS (France, UMR 8190) and IGE (France, UMR 5001).
 #
 # License: BSD 3-clause "new" or "revised" license (BSD-3-Clause).
 
-"""Common resources for compilation scripts."""
+"""Common Python resources for WRF-infra: resources for compilation."""
 
 import os
-import functools
 import argparse
-import subprocess
 import json
-
-URL_GITHUB = "https://github.com"
-URL_GROUP_WRF = "%s/wrf-model" % URL_GITHUB
-URL_WPS = "%s/WPS.git" % URL_GROUP_WRF
-URL_GROUP_POLAR = "%s/WRF-Chem-Polar" % URL_GITHUB
-URL_WRFCHEMPOLAR = "%s/WRF-Chem-Polar.git" % URL_GROUP_POLAR
-
-
-class ConvertToBoolean(argparse.Action):
-    """Action to convert command-line arguments to booleans."""
-
-    def __call__(self, _, namespace, values, option_string=None):
-        """Convert command line option value to boolean.
-
-        See https://docs.python.org/3/library/argparse.html#action-classes for
-        more details about action classes and the corresponding API.
-
-        """
-        while option_string.startswith("-"):
-            option_string = option_string[1:]
-        option_string = option_string.replace("-", "_")
-        if values.lower() in ("true", "t", "yes", "y"):
-            values = True
-        elif values.lower() in ("false", "f", "no", "n"):
-            values = False
-        else:
-            msg = f'Could not convert "{values}" to boolean.'
-            raise ValueError(msg)
-        setattr(namespace, option_string, values)
-
-
-@functools.lru_cache
-def identify_host_platform():
-    """Return the identity of the host platform.
-
-    Returns
-    -------
-    str
-        The identity of the host platform.
-
-    """
-    known_plateforms = {
-        "spirit1.ipsl.fr": "spirit",
-        "spirit2.ipsl.fr": "spirit",
-    }
-    nodename = os.uname().nodename
-    try:
-        platform = known_plateforms[nodename]
-    except KeyError:
-        msg = f"Unknown host platform: {nodename}."
-        raise NotImplementedError(msg)
-    return platform
-
-
-def process_path(path):
-    """Return a unique absolute version of given path.
-
-    Parameters
-    ----------
-    path: str | None
-        The path to process.
-
-    Returns
-    -------
-    str | None
-        The unique and absolute version of given path (or None if given path is
-        empty or None)
-
-    """
-    if path is None or path.strip() == "":
-        return None
-    return os.path.abspath(os.path.expanduser(path))
-
-
-def path_of_repo(path=None):
-    """Return path of Git repository containing given path.
-
-    Parameters
-    ----------
-    path: str
-        A path that is supposed to be located somewhere in a Git repository. If
-        None, then this function returns the path of the Git repository
-        containing this module.
-
-    Returns
-    -------
-    str
-        The path of the Git repository that contains given path.
-
-    Notes
-    -----
-    Given path does not have to exist to find the corresponding Git repository.
-    For example, if /home/myself/myrepo is an existing Git repository, then
-    path_of_repo("/home/myself/myrepo/myfile.txt") will return
-    "/home/myself/myrepo" even if myfile.txt does not exists.
-
-    """
-    abspath = os.path.abspath(__file__ if path is None else path)
-    if os.path.isdir(os.path.join(abspath, ".git")):
-        return abspath
-    parent = os.path.dirname(abspath)
-    if parent == abspath:
-        msg = "Could not determine path to git repository."
-        raise RuntimeError(msg)
-    else:
-        return path_of_repo(parent)
-
-
-def repo_is_local(repository):
-    """Return whether given repository address is local.
-
-    This function only looks at the format of the given character string.
-    Whether the repository is local or remote, this function does not check if
-    the repository exists or not.
-
-    Returns
-    -------
-    bool
-        True if given address is local, False otherwise.
-
-    """
-    return (
-        "@" not in repository
-        and not repository.startswith("http://")
-        and not repository.startswith("https://")
-    )
-
-
-def run(args, **kwargs):
-    """Run given command and arguments as a subprocess.
-
-    Parameters
-    ----------
-    args: sequence
-        The command to run and its arguments, eg. ["grep", "-v", "some text"].
-    kwargs: dict
-        These are passed "as is" to subprocess.run.
-
-    Returns
-    -------
-    subprocess.CompletedProcess
-        The result of running the command.
-
-    Raises
-    ------
-    RuntimeError
-        If the command returns a non-zero exit code.
-
-    """
-    out = subprocess.run(args, **kwargs)
-    if out.returncode:
-        msg = f"Command '{' '.join(args)}' exited with non-zero return code."
-        raise RuntimeError(msg)
-    return out
-
-
-def run_stdout(args, **kwargs):
-    """Run given command and arguments as a subprocess and return stdout.
-
-    Parameters
-    ----------
-    args: sequence
-        The command to run and its arguments, eg. ["grep", "-v", "some text"].
-    kwargs: dict
-        These are passed "as is" to subprocess.run, but cannot contain
-        "capture_output" nor "text".
-
-    Returns
-    -------
-    [str]
-        The standard output of the command (one string per line).
-
-    Raises
-    ------
-    RuntimeError
-        If the command returns a non-zero exit code.
-
-    """
-    nope_list = ("capture_output", "text")
-    forbidden_kwargs = list(filter(lambda kwarg: kwarg in nope_list, kwargs))
-    if forbidden_kwargs:
-        msg = f"Forbidden keyword argument(s): {', '.join(forbidden_kwargs)}."
-        raise ValueError(msg)
-    out = run(args, capture_output=True, text=True, **kwargs)
-    return out.stdout[:-1].split("\n")
+from . import generic
 
 
 def prepare_argparser(which):
@@ -212,13 +26,13 @@ def prepare_argparser(which):
     """
     # "which"-dependent default values
     if which == "WPS":
-        repository = URL_WPS
+        repository = generic.URL_WPS
         commit = "master"
         patches = os.path.join(
-            path_of_repo(), "compile", "patches", "WPS", "v4.6.0"
+            generic.path_of_repo(), "compile", "patches", "WPS", "v4.6.0"
         )
     elif which == "WRF":
-        repository = URL_WRFCHEMPOLAR
+        repository = generic.URL_WRFCHEMPOLAR
         commit = "polar/main"
         patches = None
     else:
@@ -258,7 +72,7 @@ def prepare_argparser(which):
     parser.add_argument(
         "--scheduler",
         help="Whether or not to compile in a scheduled job.",
-        action=ConvertToBoolean,
+        action=generic.ConvertToBoolean,
         default=True,
     )
     parser.add_argument(
@@ -277,7 +91,7 @@ def prepare_argparser(which):
             "Whether this is a dry run (dry run: clone the repository and "
             "create the compile script, but do not run the compile script)."
         ),
-        action=ConvertToBoolean,
+        action=generic.ConvertToBoolean,
         default=False,
     )
 
@@ -291,7 +105,7 @@ def prepare_argparser(which):
         parser.add_argument(
             "--parallel",
             help="Whether to compile with support for parallel computing.",
-            action=ConvertToBoolean,
+            action=generic.ConvertToBoolean,
             default=True,
         )
     elif which == "WRF":
@@ -348,36 +162,16 @@ def get_options(which):
     if opts.repository.startswith("http://"):
         msg = "We do not allow http connections (not secure)."
         raise ValueError(msg)
-    if repo_is_local(opts.repository):
-        opts.repository = process_path(opts.repository)
-    opts.destination = process_path(opts.destination)
+    if generic.repo_is_local(opts.repository):
+        opts.repository = generic.process_path(opts.repository)
+    opts.destination = generic.process_path(opts.destination)
     if "components" in opts:
         opts.components = [
             comp.strip() for comp in opts.components.split(",") if comp.strip()
         ]
-    opts.patches = process_path(opts.patches)
-    opts.sources = process_path(opts.sources)
+    opts.patches = generic.process_path(opts.patches)
+    opts.sources = generic.process_path(opts.sources)
     return opts
-
-
-def clone_and_checkout(opts):
-    """Clone the repository and checkout the required commit.
-
-    Parameters
-    ----------
-    opts: Namespace
-        The pre-processed user-defined installation options.
-
-    Raises
-    ------
-    RuntimeError
-        If the destination already exists.
-
-    """
-    if os.path.exists(opts.destination):
-        raise RuntimeError("Destination directory already exists.")
-    run([opts.git, "clone", opts.repository, opts.destination])
-    run([opts.git, "checkout", opts.commit], cwd=opts.destination)
 
 
 def format_shell_value(value):
@@ -418,7 +212,7 @@ def prepare_slurm_options(time):
         The lines of text that contain the slurm options.
 
     """
-    host = identify_host_platform()
+    host = generic.identify_host_platform()
     slurm = {
         "ntasks": "1",
         "ntasks-per-node": "1",
@@ -445,7 +239,7 @@ def write_options(opts):
     opts_dict.pop("optfile")
     opts_dict.pop("destination")
     cmd = ["git", "rev-parse", "HEAD"]
-    opts_dict["commit"] = run_stdout(cmd, cwd=opts.destination)[0]
+    opts_dict["commit"] = generic.run_stdout(cmd, cwd=opts.destination)[0]
     if "components" in opts_dict:
         opts_dict["components"] = ",".join(opts_dict["components"])
     optfile = os.path.join(opts.destination, "compile.json")
@@ -466,12 +260,12 @@ def process_patches(opts):
     if opts.patches is None:
         return
     cmd = ["find", opts.patches, "-type", "f", "-name", "*.patch"]
-    patches = [process_path(patch) for patch in run_stdout(cmd)]
+    patches = [generic.process_path(p) for p in generic.run_stdout(cmd)]
     n = len(opts.patches) + 1
     for patch in patches:
         path_in_repo = os.path.join(opts.destination, patch[n:-6])
         if os.path.exists(path_in_repo):
-            run(["patch", path_in_repo, patch])
+            generic.run(["patch", path_in_repo, patch])
         else:
             msg = f"File {path_in_repo} does not exist so cannot be patched."
             raise RuntimeError(msg)
@@ -489,8 +283,8 @@ def process_extra_sources(opts):
     if opts.sources is None:
         return
     cmd = ["find", opts.sources, "-type", "f"]
-    sources = [process_path(src) for src in run_stdout(cmd)]
+    sources = [generic.process_path(src) for src in generic.run_stdout(cmd)]
     n = len(opts.sources) + 1
     for src in sources:
         path_in_repo = os.path.join(opts.destination, src[n:])
-        run(["cp", "-v", src, path_in_repo])
+        generic.run(["cp", "-v", src, path_in_repo])
