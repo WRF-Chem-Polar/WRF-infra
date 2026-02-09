54a032206b80c5d5d39b2c5ae6003b7338242619
Lucas Bastien <134368131+lucas-ige@users.noreply.github.com>
2026-02-09 17:47:20 +0100

    Start processing the command-line arguments

--- testing/analyse-results.bash
+++ testing/analyse-results.bash
@@ -5,9 +5,51 @@
 # License: BSD 3-clause "new" or "revised" license (BSD-3-Clause).
 #
 # Jobscript to analyze the results of one or more WRF runs.
+#
+# For documentation, run:
+#
+# ./analyse-results.job --help
+#
+# (or look at the variable ${docstring} below).
 
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --time=00:30:00
 
 echo "Running analyse-results.job script"
+
+docstring=$(cat <<'EOF'
+This script runs our default analysis on one or more WRF-Chem simulations.
+
+This script is intended to be used on the directory produced by the
+script plot-vertical-profiles.py.
+
+Options:
+
+--help
+    Prints this docstring and exits without error.
+
+--work-dir (mandatory)
+    The directory where the outputs of the simulations are. This directory must
+    contain sub-directories such as:
+
+     - outputs_1/wrf_...
+     - outputs_2/wrf_...
+
+--simulations (default=all)
+    Either the keyword "all" or a comma-separated list of simulation numbers
+    to analyse (for example 1,2,4 for the first, second, and fourth
+    simulations).
+
+EOF
+)
+
+# Process command-line arguments
+
+for ((i = 1; i <= $#; i++)); do
+    arg=${!i}
+    if [[ "${arg}" == "--help" ]]; then
+        echo -e "\n${docstring}\n"
+        exit 0
+    fi
+done
