0e5d889df09bf445d31dad49c4d0d74c1acd5745
Lucas Bastien <134368131+lucas-ige@users.noreply.github.com>
2026-04-01 16:34:26 +0200

    Update copyright statements with new year

--- env/spirit.sh
+++ env/spirit.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# Copyright (c) 2025 LATMOS (France, UMR 8190) and IGE (France, UMR 5001).
+# Copyright (c) 2025-2026 LATMOS (France, UMR 8190) and IGE (France, UMR 5001).
 #
 # License: BSD 3-clause "new" or "revised" license (BSD-3-Clause).
 #
