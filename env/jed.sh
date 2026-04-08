5c793bdd4a815a71c5084fefd80ba92d947a0ff5
Anderson Da Silva <63862293+Andersyes@users.noreply.github.com>
2026-04-08 11:57:19 +0200

    Update path and comments

--- env/jed.sh
+++ env/jed.sh
@@ -11,16 +11,13 @@
 
 module purge
 
-# --- loading flex library
-ml flex/2.6.1-y4a5vd2
+module load flex/2.6.1-y4a5vd2
 export FLEX_LIB_DIR="/ssoft/spack/pinot-noir/jed/v2/spack/opt/spack/linux-rhel9-x86_64_v3/gcc-11.4.1/flex-2.6.1-y4a5vd2zgmnkdott7mvp6ogiv56khlw3/lib/"
 
-# --- loading gcc and openmpi
 module load gcc/13.2.0
 module load openmpi/5.0.3
 
-# loading the netcedf-fortran set of modules (custom installation)
-export MODULEPATH=$MODULEPATH:/work/eerl/apdasilv/wrf-chem-polar/software/libraries/gcc-v13.2.0/module_files
+export MODULEPATH=$MODULEPATH:/work/eerl/wrf-chem-polar/software/libraries/gcc-v13.2.0/module_files
 module --ignore_cache load zlib-v1.3.1.module
 module --ignore_cache load hdf5-v1.14.6_zlib-v1.3.1.module
 module --ignore_cache load netcdf-fortran-v4.6.2_netcdf-c-v4.9.3_hdf5-v1.14.6_zlib-v1.3.1.module
@@ -28,5 +25,5 @@ module --ignore_cache load netcdf-fortran-v4.6.2_netcdf-c-v4.9.3_hdf5-v1.14.6_zl
 
 conda_run="/work/eerl/apdasilv/wrf-chem-polar/software/micromamba/micromamba
            run
-           --root-prefix=/work/eerl/apdasilv/wrf-chem-polar/software/conda-envs/shared
+           --root-prefix=/work/eerl/wrf-chem-polar/software/conda-envs/shared
            --name=WRF-Chem-Polar"
