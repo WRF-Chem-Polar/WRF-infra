5b9f79842a7a35cfcc7777aa5a4f4a72f07ce08f
ruthprice <22415989+ruthprice@users.noreply.github.com>
2026-08-21 13:53:15 +0200

    Fix python command

--- run/real/jobscript_real.sh
+++ run/real/jobscript_real.sh
@@ -290,7 +290,7 @@ fi
 echo " "
 echo "-------- $SLURM_JOB_NAME: Use high-latitude erodibility data --------"
 echo " "
-${cmd_python} regrid_wrfinput_erod.py ./wrfinput_d01
+${cmd_python} hldust_regrid_wrfinput_erod.py ./wrfinput_d01
 
 #----------#
 # Finalize #
